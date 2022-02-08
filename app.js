28b1e3343dd8e492616dcbeef7405d501e155d50
shin-122-0 <98644841+shin-122-0@users.noreply.github.com>
2022-02-09 00:22:38 +0900

    2.5
    Array

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-const amIFat = null;
-let something;
-console.log(amIFat);
-console.log(something);
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const daysOfWeek = [mon , tue , wed, thu , fri , sat];
+
+console.log(daysOfWeek);
+
+const nonsense = [1, 2, "hello", false, null, true, undefined, "shinyoung"];
+
+console.log(daysOfWeek, nonsense);
+
+// Get Item from Array
+console.log(daysOfWeek[5]);
+
+// Add one more day to the array
+daysOfWeek.push(sun);
+console.log(daysOfWeek);
+
+const toBuy = ["potato", "tomato", "pizza"];
+toBuy.push("kimbab");
