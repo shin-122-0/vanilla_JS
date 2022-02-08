3da2d2c8eefbe479684237bc1ba13173b79cfd4a
shin-122-0 <98644841+shin-122-0@users.noreply.github.com>
2022-02-09 00:04:32 +0900

    2.4
    true, false, null, undefined

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
-const a = 13;
-const b = 3;
-
-let myName = "shinyoung";
-
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
-
-myName = "Han";
-
-console.log("My name is " + myName);
+const amIFat = null;
+let something;
+console.log(amIFat);
+console.log(something);
