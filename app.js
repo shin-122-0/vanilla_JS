96a4c5e05e6bda7b3f0cd1fc6831b7c1b1ae363e
shin-122-0 <98644841+shin-122-0@users.noreply.github.com>
2022-02-08 23:50:01 +0900

    1.3

---
+++ app.js
@@ -0,0 +1,13 @@
+const a = 13;
+const b = 3;
+
+let myName = "shinyoung";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "Han";
+
+console.log("My name is " + myName);
