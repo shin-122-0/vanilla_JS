40d2e0a1de01ee5eea94abd0ad27550f8d36abc1
shin-122-0 <98644841+shin-122-0@users.noreply.github.com>
2022-02-09 22:55:19 +0900

    2.7
    function, variable
    make introduce sentence

--- app.js
+++ app.js
@@ -1,28 +1,7 @@
-const playerName = "shinyoung";
-const playerPoints = 121212;
-const playerHair = "long";
-const playerWallet = false;
+function sayHello(nameOfPerson, age){
+    console.log("Hello my name is " + nameOfPerson + "and I'm " + age);
+}
 
-// comment로 설명하기?
-// player[0] == name
-// player[1] == points 
-const playertest = ["shinyoung", 1212, false, "long"];
-
-// use Object
-const player = {
-    name: "shinyoung",
-    points: 120,
-    hair: "long",
-    Wallet: false,
-};
-
-console.log(player);
-console.log(player.name);
-console.log(player["name"]);
-
-player.Wallet = true;
-player.lastName = "Han";
-console.log(player);
-
-player.points = player.points + 15;
-console.log(player.points);
+sayHello("shinyoung", 10);
+sayHello("dal", 23);
+sayHello("Han", 20);
