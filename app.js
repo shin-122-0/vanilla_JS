5180b5a745b63e47767251eb4ac1d0a344c4a10f
shin-122-0 <98644841+shin-122-0@users.noreply.github.com>
2022-02-10 00:44:17 +0900

    2.15.1
    if + else if + else

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 const age = parseInt(prompt("How old are you?") );
 
+if (isNaN(age)) {
+    /// condition === true
+    console.log("Please write a number");
+}
+else if (age < 18) {
+    /// condition === false
+    console.log("You are too young.");
+}
+else if (age >= 18 && age <= 50) {
+    console.log("You can drink.");
+}
+else if (age > 50 && age <= 80){
+    console.log("You should exercise!");
+}
+else {
+    /// condition === false agatin
+    console.log("Save your health!");
+}
 
-console.log(age);
