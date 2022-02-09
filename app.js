1871a643e5d51793209ad4b8bd1bf4ea778bb6c6
shin-122-0 <98644841+shin-122-0@users.noreply.github.com>
2022-02-10 00:25:17 +0900

    2.13
    parseInt, typeof

--- app.js
+++ app.js
@@ -1,25 +1,4 @@
-const calculator = {
-    plus: function(a, b, c, d) {
-        return a + b + c + d ;
-    },
-    times: function(a, b) {
-        return a * b;
-    },
-    minus: function(a, b) {
-        return a - b;
-    },
-    divide: function(a, b) {
-        return a / b;
-    },
-    power: function(a, b) {
-        return a ** b;
-    },
-};
+const age = parseInt(prompt("How old are you?") );
 
-const plusResult = calculator.plus(2, 3, 1, 4);
-const minusResult = calculator.minus(8, plusResult);
-const timesResult = calculator.times(10, minusResult);
-const divideResult = calculator.divide(timesResult, plusResult);
-const powerResult = calculator.power(divideResult, minusResult);
 
-console.log(plusResult);
+console.log(age);
