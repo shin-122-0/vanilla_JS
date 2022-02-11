6ba828b6fe0b2b0fc1c7243228ba69dc476131c3
shin-122-0 <98644841+shin-122-0@users.noreply.github.com>
2022-02-11 18:55:44 +0900

    3.8
    classLIst .contains/remove/add

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
-//console.dir(h1);
-
-/// === means check equality
-/// = means "IS"
-
 function handleTitleClick(){
-    const currentColor = h1.style.color;
-    ///let으로 변할 수 있는 변수를 생성. 내부는 비어있음.
-    let newColor;
-    if (currentColor === "blue") {
-        newColor = "tomato";
+    const clickedClass = "clicked";
+    if (h1.classList.contains(clickedClass)) {
+        h1.classList.remove(clickedClass);
     }
     else {
-        newColor = "blue";
+        h1.classList.add(clickedClass);
     }
-    h1.style.color = newColor;
 }
 
 h1.onclick = handleTitleClick;
