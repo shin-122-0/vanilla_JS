8ab68d78a6efa33a264eb925c2cc9cbbb0bd8963
shin-122-0 <98644841+shin-122-0@users.noreply.github.com>
2022-02-14 17:41:32 +0900

    4.5~4.6
    localstorage 이요
    setItem, getItem, removeItem
    대문자 변수
    반복구문 함수화

--- app.js
+++ app.js
@@ -4,19 +4,41 @@ const greeting = document.querySelector("#greeting");
 
 //string만 포함된 변수는 대문자로 작성 관습
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    console.log(username);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
     //greeting.innerText = "Hello " + username;
-    greeting.innerText = `Hello ${username}`;
+    //반복1 greeting.classList.remove(HIDDEN_CLASSNAME);
+    //반복2 greeting.innerText = `Hello ${username}`;
+    paintGreetings(savedUsername);
+    
+}
+
+//반복된 문장을 함수로 만들어주기
+function paintGreetings(username){
     greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
 }
 
 
-loginForm.addEventListener("submit", onLoginSubmit);
+//loginForm.addEventListener("submit", onLoginSubmit); IF문 내로 이동
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    //show the greetings
+    //반복1 greeting.classList.remove(HIDDEN_CLASSNAME);
+    //반복2 greeting.innerText = `Hello ${username}`;
+    paintGreetings(savedUsername);
+}
 
 //want the infromation
 
