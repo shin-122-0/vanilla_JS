b0af352eab47134c16db2e030ea0db65019df2bb
shin-122-0 <98644841+shin-122-0@users.noreply.github.com>
2022-02-16 22:48:04 +0900

    Delete clock.js

--- clock.js
+++
@@ -1,9 +0,0 @@
-const clock = document.querySelector("h2 #clock");
-
-//every 2 sec play this 
-function sayHello() {
-    console.log("hello");
-}
-//바로 실행한다, ms마다. (5sec = 5000)
-setInterval(sayHello, 5000);
-
